3d183367f8fb9d452f0b95717c65d3e101cb841b
retz8 <ekkicb71@gmail.com>
2023-08-16 00:57:27 +0900

    base styling

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+// < 768px
+
+import React from "react";
+
+export default function Header() {
+  return (
+    <div
+      className="flex justify-center py-3 px-2
+      border-b border-gray-150"
+    >
+      <p>Tactic Board</p>
+    </div>
+  );
+}
