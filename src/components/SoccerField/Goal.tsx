8611f6b6f2c827b9c0d9be639b756a5ecfee981b
retz8 <ekkicb71@gmail.com>
2023-08-16 15:45:57 +0900

    fix goal

--- src/components/SoccerField/Goal.tsx
+++ src/components/SoccerField/Goal.tsx
@@ -7,14 +7,9 @@ type Props = {
 export default function Goal({ direction }: Props) {
   return (
     <div
-      className={`w-full h-full flex items-center ${
-        direction === "left" ? "left-0" : "rotate-180 right-0"
-      }`}
-    >
-      <div
-        className="h-[10.76%] aspect-[2.44/7.32] translate-x-9 
-      border-l-2 border-t-2 border-b-2 border-white"
-      />
-    </div>
+      className={`h-[10.76%] aspect-[2.44/7.32] 
+      ${direction === "left" ? "rotate-0" : "rotate-180"}
+      border-l-2 border-t-2 border-b-2 border-white`}
+    />
   );
 }
